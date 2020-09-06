0cf2ed1c2f362b8b02e618280856c8596ebbcd30
Sergey Esipenko <s.esipenko@gmail.com>
2020-09-06 16:11:35 +0700

    OS-1: database

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from '@hapi/joi';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    validationSchema: Joi.object({
+      POSTGRES_HOST: Joi.string().required(),
+      POSTGRES_PORT: Joi.number().required(),
+      POSTGRES_USER: Joi.string().required(),
+      POSTGRES_PASSWORD: Joi.string().required(),
+      POSTGRES_DB: Joi.string().required(),
+    })
+  }), TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: async (configService: ConfigService) => ({
+      type: "postgres",
+      host: configService.get("POSTGRES_HOST"),
+      port: configService.get("POSTGRES_PORT"),
+      username: configService.get("POSTGRES_USER"),
+      password: configService.get("POSTGRES_PASSWORD"),
+      database: configService.get("POSTGRES_DB"),
+      synchronize: false,
+    }),
+    inject: [ConfigService]
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
